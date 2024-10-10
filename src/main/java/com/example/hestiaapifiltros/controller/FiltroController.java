a07480c86b86382ccef3cbfc586d385a518b9b2f
sophiekumagai <sophie.satie20@gmail.com>
2024-10-10 08:35:53 -0300

    adicionando status

--- src/main/java/com/example/hestiaapifiltros/controller/FiltroController.java
+++ src/main/java/com/example/hestiaapifiltros/controller/FiltroController.java
@@ -10,7 +10,9 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 @ControllerAdvice
@@ -47,5 +49,22 @@ public class FiltroController {
     })
     public List<Filtro> getAllFiltrosByCategoria(@Parameter(name = "categoria", description = "É necessário uma categoria", required = true) @PathVariable String categoria) {
         return filtroService.getByCategoria(categoria);
+    }
+    @GetMapping("/status")
+    @Operation(summary = "Verifica o status da API",
+            description = "Retorna um map contendo o status da API")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Status retornado com sucesso!",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = Filtro.class))),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor",
+                    content = @Content())
+    })
+    public Map<String, String> getStatus() {
+        // Cria um mapa com informações de status
+        Map<String, String> status = new HashMap<>();
+        status.put("status", "API is running");
+
+        // Retorna o mapa como JSON
+        return status;
     }
 }
